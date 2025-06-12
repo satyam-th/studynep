5ca7df80ee613fba77ebf2672da4f32440835933
satyamthapa <dipsonthapa211@gmail.com>
2025-06-12 16:03:10 +0545

    chnage lot of things in style

--- web/src/script.js
+++ web/src/script.js
@@ -1,5 +1,17 @@
-//navbar for phone
+// Navbar for phone
 function toggleMenu() {
-  const navMenu = document.querySelector('.nav-menu');
-  navMenu.classList.toggle('show');
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('show');
 }
+
+// Save and restore scroll position
+window.addEventListener('beforeunload', () => {
+    sessionStorage.setItem('scrollPosition', window.scrollY);
+});
+
+window.addEventListener('load', () => {
+    const scrollPosition = sessionStorage.getItem('scrollPosition');
+    if (scrollPosition) {
+        window.scrollTo(0, parseInt(scrollPosition, 10));
+    }
+});
